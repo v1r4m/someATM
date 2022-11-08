41786a171067b868a79164cba237deb682b124d0
v1r4m <choyoung7789@gmail.com>
2022-11-08 17:58:00 +0900

    main page made

--- atm/urls.py
+++ atm/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('balance/<int:userid>/', views.Balance_list, name='Balance_list'),
-    path('insert/<int:cardNumber>/', views.selectCard, name='selectCard'),
-    path('pin/<int:carNumber>/<int:pinNumber>/', views.MatchCardAndPin, name='MatchCardAndPin'),
-    path('deposit/<int:cardNumber>/<int:money>', views.Deposit, name='Deposit'),
+    path('', views.mainpage, name='manepage'),
+    path('balance/<int:cardnum>/<int:input>/', views.Balance_list, name='Balance_list'),
+    path('insert/<int:cardnum>/', views.selectCard, name='selectCard'),
+    path('pin/<int:cardnum>/<int:input>/', views.MatchCardAndPin, name='MatchCardAndPin'),
+    path('deposit/<int:cardnum>/<int:money>', views.Deposit, name='Deposit'),
 ]
 #is it safe to pass the insecured information through parameter in URL?
 #if we use django in only backend system...
--- atm/views.py
+++ atm/views.py
@@ -2,29 +2,37 @@ from django.shortcuts import render
 from .models import Card, User
 import hashlib
 
-def selectCard(cardN):
-    selectedCard = Card.objects.filter(cardNumber = cardN)
+def mainpage(request):
+    return render(request, 'atm/main.html')
+
+def selectCard(request,cardnum):
+    selectedCard = Card.objects.filter(cardNumber = cardnum)
     selectedCard[0].isInserted = True
     return selectedCard[0]
 
 
-def MatchCardAndPin(cardnum, input):
-    password = (User.objects.filter(cardUser=cardnum))[0].PinNumber
+def MatchCardAndPin(request,cardnum, input):
+    userIdt = (Card.objects.filter(cardNumber=cardnum))[0].cardUser
+    print(userIdt)
+    password = (User.objects.filter(userId=userIdt))[0].PinNumber
     inputhash = hashlib.sha256(input.encode())
     if password==inputhash:
         return True
     else:
         return False
 
-def Balance_list(request, cardnum):
+def Balance_list(request, cardnum, input):
     Balances = Card.objects.filter(cardNumber=cardnum)
-    return render(request, 'atm/deposit_list.html', {'Balances':Balances})
+    if MatchCardAndPin(request,cardnum,input):
+        return render(request, 'atm/deposit_list.html', {'Balances':Balances})
+    else:
+        return "Pin is wrong"
 
-def Deposit(cardnum, money):
+def Deposit(request,cardnum, money):
     card = Card.objects.filter(cardNumber = cardnum)
     card[0].Balance = card[0].Balance+money
 
-def Withdraw(cardnum, money):
+def Withdraw(request,cardnum, money):
     card = Card.objects.filter(cardNumber=cardnum)
     leftmoney = card[0].Deposit
     if leftmoney > money:
